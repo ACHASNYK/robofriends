3b53032a27db16fffb3abd32b09b0f0aab1514ad
Achasnyk <a.chasnyk@gmail.com>
2022-01-04 21:42:12 +0200

    2

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,8 @@ import React from "react";
 const Card = (props) => {
     return (
         
-        <div className = "bg-light-green dib br3 pa3 ma2 bg-animate hover-bg-white black grow bw2 shadow-5">
-            <img alt = "hui" src="https://robohash.org/ДмитроВалентинович?200x200" />
+        <div className = " tc bg-light-green dib br3 pa3 ma2 bg-animate hover-bg-white black grow bw2 shadow-5">
+            <img alt="hui" src={'https://robohash.org/${props.id}?200x200'} />
             <div>
                 <h2>{ props.name}}</h2>
                 <p>{props.slogan}</p>
